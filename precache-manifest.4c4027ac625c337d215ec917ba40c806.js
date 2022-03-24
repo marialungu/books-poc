324d9721b60919c32af5622437935478e4e9aa86
Ana Maria Gabriela Lungu <heymarialungu@gmail.com>
2022-03-24 18:37:31 +0200

    Updates

---
+++ precache-manifest.4c4027ac625c337d215ec917ba40c806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84cc5f21289e975d774b",
+    "url": "/books-poc/static/css/main.c8c9dc78.chunk.css"
+  },
+  {
+    "revision": "84cc5f21289e975d774b",
+    "url": "/books-poc/static/js/main.84cc5f21.chunk.js"
+  },
+  {
+    "revision": "d4dc7e49c91e923899c9",
+    "url": "/books-poc/static/css/1.e3fcd3e2.chunk.css"
+  },
+  {
+    "revision": "d4dc7e49c91e923899c9",
+    "url": "/books-poc/static/js/1.d4dc7e49.chunk.js"
+  },
+  {
+    "revision": "91d9870274b1fffd47b9",
+    "url": "/books-poc/static/js/runtime~main.91d98702.js"
+  },
+  {
+    "revision": "693cc1122538092a7a0e233c76396d14",
+    "url": "/books-poc/index.html"
+  }
+];
