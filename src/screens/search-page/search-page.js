b22948dc4e91c1f58d1587230a4d0c0f60c21ce3
Ana Maria Gabriela Lungu <heymarialungu@gmail.com>
2022-03-25 12:38:44 +0200

    add loader

--- src/screens/search-page/search-page.js
+++ src/screens/search-page/search-page.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { InstantSearch, SearchBox, Configure } from 'react-instantsearch-dom';
+import {
+  InstantSearch,
+  SearchBox,
+  Configure,
+  connectStateResults,
+} from 'react-instantsearch-dom';
+import RotateLoader from 'react-spinners/RotateLoader';
 
 import Metas from '../../components/metas';
 import BooksFilters from '../../components/books-filters';
@@ -7,6 +13,30 @@ import BookContainer from '../../components/book-container/book-container';
 import './search-page.css';
 import { searchClient } from '../../constants/init-clients';
 import { BOOKS_INDEX } from '../../constants/app-details';
+
+const FragmentedPage = () => (
+  <>
+    <Metas />
+    <div className="results">
+      <div className="left-panel">
+        <BooksFilters />
+      </div>
+      <div className="search-panel">
+        <BookContainer />
+      </div>
+    </div>
+  </>
+);
+
+const PageContent = connectStateResults(({ isSearchStalled }) =>
+  isSearchStalled ? (
+    <div className="loader">
+      <RotateLoader color={'#9698c3'} size={11} />
+    </div>
+  ) : (
+    <FragmentedPage />
+  )
+);
 
 const SearchPage = () => (
   <div className="container">
@@ -19,15 +49,7 @@ const SearchPage = () => (
         }}
         showLoadingIndicator={true}
       />
-      <Metas />
-      <div className="results">
-        <div className="left-panel">
-          <BooksFilters />
-        </div>
-        <div className="search-panel">
-          <BookContainer />
-        </div>
-      </div>
+      <PageContent />
     </InstantSearch>
   </div>
 );
