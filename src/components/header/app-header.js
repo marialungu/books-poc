641023c0ae77938d96b29f1def6a2d10299d91b0
Ana Maria Gabriela Lungu <heymarialungu@gmail.com>
2022-03-28 18:44:09 +0300

    add logo link

--- src/components/header/app-header.js
+++ src/components/header/app-header.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './app-header.css';
-const AppHeader = ({children}) => {
-  return (
-    <div>
-      <div className="header-bkgd">
-        <div className="header-container">
-          <div className="logo">ReadNook</div>
-        </div>
-      </div>
+import { Link } from 'react-router-dom';
 
-      {children}
+const AppHeader = ({ children }) => (
+  <Link to={{ pathname: '/' }}>
+    <div className="header-bkgd">
+      <div className="header-container">
+        <div className="logo">ReadNook</div>
+      </div>
     </div>
-  )
-}
+    {children}
+  </Link>
+);
 
 export default AppHeader;
